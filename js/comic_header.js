3f4272296fd93e9d20b193889cf842bd64e17864
dr-pingus <79774502+dr-pingus@users.noreply.github.com>
2021-09-24 08:11:51 -0500

    Update comic_header.js
    adjusted header

--- js/comic_header.js
+++ js/comic_header.js
@@ -10,6 +10,7 @@ document.write(`
             <!--<a href="about.html">ABOUT</a> |
             <a href="characters.html">CHARACTERS</a>-->
 			<a href="https://patreon.com/artifly">PATREON</a>
+			<a href="https://artifly.cfw.me">MAIN SITE</a>
         </div>
     </header>
 `);
