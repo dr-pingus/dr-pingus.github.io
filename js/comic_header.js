71e8abd6eee9f52d0c1419c14a66efa8338411e8
dr-pingus <79774502+dr-pingus@users.noreply.github.com>
2021-11-01 14:24:00 -0600

    Update comic_header.js
    VOTE LINK

--- js/comic_header.js
+++ js/comic_header.js
@@ -10,7 +10,8 @@ document.write(`
             <!--<a href="about.html">ABOUT</a> |
             <a href="characters.html">CHARACTERS</a>-->
 			<a href="https://patreon.com/artifly">PATREON</a> | 
-			<a href="https://artifice.cf/feed.rss">SUBSCRIBE</a>
+			<a href="https://artifice.cf/feed.rss">SUBSCRIBE</a> |
+			<a href="https://www.topwebcomics.com/vote/25875">VOTE</a>
         </div>
     </header>
 `);
