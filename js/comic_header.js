2fc1193a7eae5f174535e55d87d4262e368a6d92
dr-pingus <79774502+dr-pingus@users.noreply.github.com>
2021-12-17 11:19:10 -0600

    Added character page
    Character descriptions WIP

--- js/comic_header.js
+++ js/comic_header.js
@@ -7,8 +7,8 @@ document.write(`
         <div id="nav">
             <a href="index.html">HOME</a> |
             <a href="archive.html">ARCHIVE</a> |
-            <!--<a href="about.html">ABOUT</a> |
-            <a href="characters.html">CHARACTERS</a>-->
+            <!--<a href="about.html">ABOUT</a> |-->
+            <a href="characters.html">CHARACTERS</a> |
 			<a href="https://patreon.com/artifly">PATREON</a> | 
 			<a href="https://www.patreon.com/posts/56257753">GALLERY</a> |			
 			<!--<a href="https://artifice.cf/feed.rss">SUBSCRIBE</a> |-->
