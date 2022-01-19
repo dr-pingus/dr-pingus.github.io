0e9e6def7ccc60d5623ba6e8e0634583b6bfcfe5
dr-pingus <79774502+dr-pingus@users.noreply.github.com>
2022-01-18 20:06:33 -0600

    Update comic_header.js

--- js/comic_header.js
+++ js/comic_header.js
@@ -5,15 +5,15 @@ document.write(`
         <a href="index.html"><img src="./img/logo.png" alt="" /></a> 
 
         <div id="nav">
-            <a href="index.html">HOME</a> |
-            <a href="archive.html">ARCHIVE</a> |
+            &#8226;<a href="index.html">HOME</a>&#8226;
+            &#8226;<a href="archive.html">ARCHIVE</a>&#8226;
             <!--<a href="about.html">ABOUT</a> |-->
-            <a href="characters.html">CHARACTERS</a> |
-			<a href="https://patreon.com/artifly">PATREON</a> | 
-			<a href="https://www.patreon.com/posts/56257753">GALLERY</a> |			
+            &#8226;<a href="characters.html">CHARACTERS</a>&#8226;
+			&#8226;<a href="https://patreon.com/artifly">PATREON</a>&#8226;
+			&#8226;<a href="https://www.patreon.com/posts/56257753">GALLERY</a>&#8226;	
 			<!--<a href="https://artifice.cf/feed.rss">SUBSCRIBE</a> |-->
-			<a href="https://www.topwebcomics.com/vote/25875">VOTE</a> |
-			<a href="https://twitter.com/artiflycomic/">TWITTER</a>
+			&#8226;<a href="https://www.topwebcomics.com/vote/25875">VOTE</a>&#8226;
+			&#8226;<a href="https://twitter.com/artiflycomic/">TWITTER</a>&#8226;
         </div>
     </header>
 `);
