6052784d54d3846c3c50f93771d61d4e2bc20842
dr-pingus <79774502+dr-pingus@users.noreply.github.com>
2022-01-22 01:01:56 -0600

    minor fixes

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -1,6 +1,6 @@
 document.write(`
     <footer align="center">
-        <p>© 2019 - 2021 Dr. P</p>
+        <p>© 2019 - 2022 Dr. P</p>
         <p><strong>Powered by</strong></p>
         <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a>
     </footer>
