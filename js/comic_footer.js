7f823d3f324774a3cc9b69db8eeed430bec62285
dr-pingus <79774502+dr-pingus@users.noreply.github.com>
2022-10-15 02:44:14 -0500

    paypal donation link
    updated paypal link and image

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -3,20 +3,7 @@ document.write(`
         <p>© 2019 - 2022 Dr. P</p>
         <p><strong>Powered by</strong></p>
         <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a><br><br>
-		<div id="donate-button-container">
-			<div id="donate-button"></div>
-			<script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
-			<script>
-			PayPal.Donation.Button({
-			env:'production',
-			hosted_button_id:'9EH3F73343E2E',
-			image: {
-			src:'https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif',
-			alt:'Donate with PayPal button',
-			title:'PayPal - The safer, easier way to pay online!',
-			}
-			}).render('#donate-button');
-			</script>
-		</div>
+		<br>
+		<p align="center"><a href="https://www.paypal.com/donate/?hosted_button_id=9EH3F73343E2E"><img src="img/paypal-donate-button.png" height = "80px" /></a></p>
     </footer>
 `);
