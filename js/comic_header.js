0cf9856581b66dcaaa5005d95a74d684a08a76e4
dr-pingus <79774502+dr-pingus@users.noreply.github.com>
2022-10-27 23:23:29 -0500

    Update comic_header.js

--- js/comic_header.js
+++ js/comic_header.js
@@ -13,7 +13,6 @@ document.write(`
 			&#8226;<a href="https://www.patreon.com/posts/56257753">GALLERY</a>&#8226;-->	
 			<!--<a href="https://artifice.cf/feed.rss">SUBSCRIBE</a> |-->
 			&#8226;<a href="https://www.topwebcomics.com/vote/25875">VOTE</a>&#8226;
-			&#8226;<a href="https://twitter.com/artiflycomic/">TWITTER</a>&#8226;
 			&#8226;<a href="https://www.deviantart.com/pang-md">DEVIANTART</a>&#8226;
         </div>
     </header>
